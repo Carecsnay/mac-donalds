7d63830137353d1b39dae3138979aacb3088e714
Bruno Desktop <carecsnay1@gmail.com>
2025-02-19 20:33:34 -0300

    feat: add remote pattern for external images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{ hostname: "u9a6wmr3as.ufs.sh" }], //assegurando que o host das imagens serão permitidos serem carregados
+  },
 };
 
 export default nextConfig;
