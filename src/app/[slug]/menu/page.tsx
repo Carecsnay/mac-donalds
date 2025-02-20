5039eb9b00c2dbada72b67f36dc56226442d3000
Bruno Desktop <carecsnay1@gmail.com>
2025-02-20 19:00:24 -0300

    feat: adding cover and buttons to restaurant

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,5 +1,8 @@
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/prisma";
 
 interface RestaurantMenuPageProps {
@@ -21,13 +24,42 @@ const RestaurantMenuPage = async ({
     return notFound();
   }
   const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+
   return (
     <h1>
-      {slug} {consumptionMethod}
+      <div>
+        <div className="relative h-[250px] w-full">
+          <Button
+            variant="secondary"
+            size="icon"
+            className="absolute left-4 top-4 z-50 rounded-full"
+          >
+            <ChevronLeftIcon />
+          </Button>
+
+          <Image
+            src={restaurant?.coverImageUrl}
+            alt={restaurant.name}
+            fill
+            className="object-cover"
+          />
+
+          <Button
+            variant="secondary"
+            size="icon"
+            className="absolute right-4 top-4 z-50 rounded-full"
+          >
+            <ScrollTextIcon />
+          </Button>
+        </div>
+      </div>
     </h1>
   );
 };
 
 export default RestaurantMenuPage;
 
-// http://localhost:3000/fsw-foods/menu?consumptionMethod=TAKEAWAY
+// http://localhost:3000/fsw-donalds/menu?consumptionMethod=dine_in
