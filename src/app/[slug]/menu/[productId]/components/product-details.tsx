69bcddcfd26d833406275cd124117a44dc1dfa1e
Bruno Desktop <carecsnay1@gmail.com>
2025-02-22 09:00:57 -0300

    feat: add about and ingredients section

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Prisma } from "@prisma/client";
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -52,6 +52,7 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
         </p>
       </div>
 
+      {/* preço e quantidade */}
       <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
 
       <div className="flex items-center justify-between">
@@ -75,6 +76,21 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
             <ChevronRightIcon />
           </Button>
         </div>
+      </div>
+
+      {/* Sobre */}
+      <div className="mt-6 space-y-3">
+        <h4 className="font-semibold">Sobre</h4>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+      </div>
+
+      {/* Ingredientes */}
+      <div className="mt-6 space-y-3">
+        <div className="flex items-center gap-1">
+          <ChefHatIcon size={18} />
+          <h4 className="font-semibold">Ingredientes</h4>
+        </div>
+        <p className="text-sm text-muted-foreground">{product.ingredients}</p>
       </div>
     </div>
   );
