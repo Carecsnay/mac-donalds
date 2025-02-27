3627acb9ccc21f1c7fe18a0e50ce75988225bdc2
Bruno Desktop <carecsnay1@gmail.com>
2025-02-26 22:20:22 -0300

    feat: add initial server action

---
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { ConsumptionMethod } from "@prisma/client";
+
+import { db } from "@/lib/prisma";
+
+import { removeCpfPunctuation } from "../helpers/validade-cpf";
+
+interface CreateOrderInput {
+  customerName: string;
+  customerCpf: string;
+  products: Array<{
+    id: string;
+    quantity: number;
+  }>;
+  consumptionMethod: ConsumptionMethod;
+  restaurantId: string;
+}
+
+//Imagina que é algo que vai rondar no servidor (API).
+export const createOrder = async (input: CreateOrderInput) => {
+  const productsWithPrices = await db.product.findMany({
+    where: {
+      id: {
+        in: input.products.map((product) => product.id),
+      },
+    },
+  });
+
+  const productsWithPricesAndQuantities = input.products.map((product) => ({
+    productId: product.id,
+    quantity: product.quantity,
+    price: productsWithPrices.find((p) => p.id === product.id)!.price,
+  }));
+
+  await db.order.create({
+    data: {
+      status: "PENDING",
+      customerName: input.customerName,
+      customerCpf: removeCpfPunctuation(input.customerCpf), //salva cpf sem pontuação
+      orderProducts: {
+        createMany: {
+          data: productsWithPricesAndQuantities,
+        },
+      },
+      total: productsWithPricesAndQuantities.reduce(
+        (acc, product) => acc + product.price * product.quantity,
+        0,
+      ),
+      consumptionMethod: input.consumptionMethod,
+      restaurantId: input.restaurantId,
+    },
+  });
+};
