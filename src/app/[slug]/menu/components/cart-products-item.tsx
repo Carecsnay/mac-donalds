4eee71b8e26a2f4bd259d103bd3237e68981642e
Bruno Desktop <carecsnay1@gmail.com>
2025-03-01 08:46:42 -0300

    refactor: add spaces on products on list

--- src/app/[slug]/menu/components/cart-products-item.tsx
+++ src/app/[slug]/menu/components/cart-products-item.tsx
@@ -18,7 +18,7 @@ const CartProductItem = ({ product }: CartProductItemProps) => {
     useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 space-y-10">
         {/* ESQ */}
         <div className="relative h-20 w-20 rounded-xl bg-gray-100">
           <Image src={product.imageUrl} alt={product.name} fill></Image>
